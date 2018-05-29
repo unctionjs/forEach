7c65f20b218457b30938a35cb8f38357a48e733f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-28 17:34:51 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-import fromFunctorToPairs from "@unction/fromfunctortopairs"
-
-
-import type {UnaryFunctionType} from "types"
-import type {KeyType} from "types"
-import type {ValueType} from "types"
-
-export default function forEach (unction: ValueType => KeyType => mixed): UnaryFunctionType {
-  return function forEachUnction (functor: FunctorType): FunctorType {
+import fromFunctorToPairs from "@unction/fromfunctortopairs";
+export default function forEach(unction) {
+  return function forEachUnction(functor) {
     if (typeof functor.forEach === "function") {
-      functor.forEach((value: ValueType, key: KeyType) => {
-        unction(value)(key)
-      })
-
-      return functor
+      functor.forEach((value, key) => {
+        unction(value)(key);
+      });
+      return functor;
     }
 
-    return fromFunctorToPairs(functor).forEach(([key, value]: [KeyType | void, ValueType]) => {
-      unction(value)(key)
-    })
-  }
+    return fromFunctorToPairs(functor).forEach(([key, value]) => {
+      unction(value)(key);
+    });
+  };
 }
