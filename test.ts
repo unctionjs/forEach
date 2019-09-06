068094576c9b4d86047e20a1a99a97ec0c949c6e
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:45 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,48 @@
+/* eslint-disable no-magic-numbers, no-undefined, flowtype/require-variable-type */
+import { spy } from "sinon";
+
+import forEach from "./index.ts";
+
+const unction = spy(([value, key]) => [value, key]);
+
+const sample = (value) => (key) => unction([value, key]);
+
+test("Array", () => {
+  forEach(sample)(["a", "b", "c"]);
+
+  expect(unction.calledWith(["a", 0])).toBeTruthy();
+  expect(unction.calledWith(["b", 1])).toBeTruthy();
+  expect(unction.calledWith(["c", 2])).toBeTruthy();
+});
+
+test("Object", () => {
+  forEach(sample)({
+    aaa: "1",
+    bbb: "2",
+    ccc: "3",
+  });
+
+  expect(unction.calledWith(["1", "aaa"])).toBeTruthy();
+  expect(unction.calledWith(["2", "bbb"])).toBeTruthy();
+  expect(unction.calledWith(["3", "ccc"])).toBeTruthy();
+});
+
+test("Map", () => {
+  forEach(sample)(new Map([
+    ["a", "b"],
+    ["c", "d"],
+  ]));
+
+  expect(unction.calledWith(["b", "a"])).toBeTruthy();
+  expect(unction.calledWith(["d", "c"])).toBeTruthy();
+});
+
+test("Set", () => {
+  forEach(sample)(new Set([
+    "a",
+    "b",
+  ]));
+
+  expect(unction.calledWith(["a", "a"])).toBeTruthy();
+  expect(unction.calledWith(["b", "b"])).toBeTruthy();
+});
