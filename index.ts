47d962ff752f59e0927a7ef98d8e6f9d65b75c6c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-29 09:49:49 -0800

    Removing more bad types

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import fromFunctorToPairs from "@unction/fromfunctortopairs";
 import {MapperFunctionType} from "./types";
 
 export default function forEach<A, B> (unction: MapperFunctionType<A, B>) {
-  return function forEachUnction (enumerable: Array<A> | Set<A> | RecordType<unknown, A> | string) {
+  return function forEachUnction (enumerable: Array<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string) {
     if (typeof enumerable.forEach === "function") {
       enumerable.forEach((value: A) => {
         unction(value);
